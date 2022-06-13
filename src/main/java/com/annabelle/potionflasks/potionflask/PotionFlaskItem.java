e309b66fbbb2e88a68ec166a2d5c01f479625bd8
Annabelle Lewis <32372884+AnnabelleLewis@users.noreply.github.com>
2022-06-12 18:28:54 -0700

    Added todo changes to Potion flask

--- src/main/java/com/annabelle/potionflasks/potionflask/PotionFlaskItem.java
+++ src/main/java/com/annabelle/potionflasks/potionflask/PotionFlaskItem.java
@@ -1,11 +1,65 @@
 package com.annabelle.potionflasks.potionflask;
 
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.PotionItem;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.gameevent.GameEvent;
 
 public class PotionFlaskItem extends PotionItem {
     private static final int DRINK_DURATION = 16;
 
     public PotionFlaskItem(Properties p_42979_) {
         super(p_42979_);
+    }
+
+    public ItemStack finishUsingItem(ItemStack pStack, Level pLevel, LivingEntity pEntityLiving) {
+        Player player = pEntityLiving instanceof Player ? (Player)pEntityLiving : null;
+        if (player instanceof ServerPlayer) {
+            CriteriaTriggers.CONSUME_ITEM.trigger((ServerPlayer)player, pStack);
+        }
+
+        if (!pLevel.isClientSide) {
+            for(MobEffectInstance mobeffectinstance : PotionUtils.getMobEffects(pStack)) {
+                if (mobeffectinstance.getEffect().isInstantenous()) {
+                    mobeffectinstance.getEffect().applyInstantenousEffect(player, player, pEntityLiving, mobeffectinstance.getAmplifier(), 1.0D);
+                } else {
+                    pEntityLiving.addEffect(new MobEffectInstance(mobeffectinstance));
+                }
+            }
+        }
+
+        // TODO: Reduce flask fill level
+        /*
+        if (player != null) {
+            player.awardStat(Stats.ITEM_USED.get(this));
+            if (!player.getAbilities().instabuild) {
+                pStack.shrink(1);
+            }
+        }
+        */
+
+        if (player == null || !player.getAbilities().instabuild) {
+            // TODO: if fill level is 0, give player empty flask
+            /*
+            if (pStack.isEmpty()) {
+                return new ItemStack(Items.GLASS_BOTTLE);
+            }
+
+            if (player != null) {
+                player.getInventory().add(new ItemStack(Items.GLASS_BOTTLE));
+            }
+            */
+        }
+
+        pLevel.gameEvent(pEntityLiving, GameEvent.DRINKING_FINISH, pEntityLiving.eyeBlockPosition());
+        return pStack;
     }
 }
