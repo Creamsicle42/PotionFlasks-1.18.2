d2f5090b718f3262eca1ad61386d4bee547f9e97
Annabelle Lewis <32372884+AnnabelleLewis@users.noreply.github.com>
2022-06-18 07:56:52 -0700

    Added lingering flask item class

--- src/main/java/com/annabelle/potionflasks/lingeringflask/LingeringFlask.java
+++ src/main/java/com/annabelle/potionflasks/lingeringflask/LingeringFlask.java
@@ -1,37 +1,22 @@
-package com.annabelle.potionflasks.splashingflask;
+package com.annabelle.potionflasks.lingeringflask;
 
 import com.annabelle.potionflasks.ItemRegistry;
 import com.annabelle.potionflasks.config.PotionFlasksCommonConfig;
+import com.annabelle.potionflasks.splashingflask.SplashPotionFlaskItem;
 import net.minecraft.stats.Stats;
 import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.effect.MobEffect;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.ThrownPotion;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.SplashPotionItem;
-import net.minecraft.world.item.alchemy.PotionUtils;
-import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.item.LingeringPotionItem;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.EntityHitResult;
-import net.minecraft.world.phys.HitResult;
 
-import javax.annotation.Nullable;
-import java.util.List;
-
-public class SplashPotionFlaskItem extends SplashPotionItem {
-    public SplashPotionFlaskItem(Properties pProperties) {
+public class LingeringFlask extends LingeringPotionItem {
+    public LingeringFlask(Properties pProperties) {
         super(pProperties);
     }
-
-    public static int getMaxFillLevel(){return PotionFlasksCommonConfig.FLASK_MAX_FILL_LEVEL.get();}
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
@@ -66,13 +51,6 @@ public class SplashPotionFlaskItem extends SplashPotionItem {
 
     public ItemStack getEmptyItemStack(){return new ItemStack(ItemRegistry.EMPTY_SPLASH_POTION_FLASK.get());}
 
-    @Override
-    public void inventoryTick(ItemStack pStack, Level pLevel, Entity pEntity, int pSlotId, boolean pIsSelected) {
-        if(pStack.getTag().getInt("potionflasks:fill_level") < 0){
-            //pEntity.setItemSlot(getEquipmentSlot(pStack) ,new ItemStack(ItemRegistry.EMPTY_SPLASH_POTION_FLASK.get()));
-        }
-    }
-
     public boolean isBarVisible(ItemStack pStack) {
         return pStack.getTag().getInt("potionflasks:fill_level") != 0;
     }
@@ -88,7 +66,5 @@ public class SplashPotionFlaskItem extends SplashPotionItem {
         float f = Math.max(0.0F, (float)pStack.getTag().getInt("potionflasks:fill_level") / (float)PotionFlasksCommonConfig.FLASK_MAX_FILL_LEVEL.get());
         return Mth.hsvToRgb(f / 3.0F, 1.0F, 1.0F);
     }
-
-
 
 }
